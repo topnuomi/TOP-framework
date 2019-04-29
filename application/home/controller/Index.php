10ff897daf9f616eb5f084a535abbad717041869
topnuomi <1130395124@qq.com>
2019-04-29 20:56:03 +0800

    修改Twig支持

--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -5,7 +5,7 @@ namespace application\home\controller;
 class Index extends Common {
     
     public function index() {
-        return $this->fetch('Index/index', [
+        return $this->fetch('', [
             'lists' => [
                 0, 1, 2, 3, 4, 5, 6
             ]
