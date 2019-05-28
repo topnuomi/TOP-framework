04107a40c538f081dd1b66b44b8e4fd0b7229732
topnuomi <1130395124@qq.com>
2019-05-28 16:23:14 +0800

    更改命令行路由访问位置

--- framework/library/route/Command.php
+++ framework/library/route/Command.php
@@ -5,27 +5,29 @@ use framework\library\route\ifs\RouteIfs;
 
 class Command implements RouteIfs {
 
+    // 模块
+    public $module = '';
+
+    // 控制器
+    public $ctrl = '';
+
+    // 动作
+    public $action = '';
+
     /**
-     * // 暂时就这样吧（逃...
-     * 
-     * {@inheritdoc}
-     *
-     * @see \system\core\route\ifs\RouteIfs::processing()
+     * 暂时就这样吧（逃...
      */
     public function processing() {
         // TODO Auto-generated method stub
         $this->module = $this->module();
         $this->ctrl = $this->ctrl();
-        $this->className = 'app\\' . $this->module . '\\controller\\' . $this->ctrl;
+        $this->className = APPNS . '\\' . $this->module . '\\controller\\' . $this->ctrl;
         $this->action = $this->action();
         $this->param = $this->param();
     }
 
     /**
      *
-     * {@inheritdoc}
-     *
-     * @see \system\core\route\ifs\RouteIfs::module()
      */
     public function module() {
         // TODO Auto-generated method stub
@@ -34,9 +36,6 @@ class Command implements RouteIfs {
 
     /**
      *
-     * {@inheritdoc}
-     *
-     * @see \system\core\route\ifs\RouteIfs::ctrl()
      */
     public function ctrl() {
         // TODO Auto-generated method stub
@@ -45,9 +44,6 @@ class Command implements RouteIfs {
 
     /**
      *
-     * {@inheritdoc}
-     *
-     * @see \system\core\route\ifs\RouteIfs::action()
      */
     public function action() {
         // TODO Auto-generated method stub
@@ -56,9 +52,6 @@ class Command implements RouteIfs {
 
     /**
      *
-     * {@inheritdoc}
-     *
-     * @see \system\core\route\ifs\RouteIfs::params()
      */
     public function param() {
         // TODO Auto-generated method stub
